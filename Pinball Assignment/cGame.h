5030581475747e715f41c80be3efc2d017c996ac
Ruari026 <36458213+Ruari026@users.noreply.github.com>
2018-11-13 21:50:32 +0000

    Initial Setup - Addition of NuGet SDL2 Packages - Project System Build System Set

---
+++ Pinball Assignment/cGame.h
@@ -0,0 +1,52 @@
+#ifndef _GAME_H
+#define _GAME_H
+
+/*
+==================================================================================
+cGame.h
+==================================================================================
+*/
+
+#include <SDL.h>
+
+// Game specific includes
+#include "rocketGame.h"
+
+
+using namespace std;
+
+class cGame
+{
+public:
+	cGame();
+
+	void initialise(SDL_Window* theSDLWND, SDL_Renderer* theRenderer);
+	void run(SDL_Window* theSDLWND, SDL_Renderer* theRenderer);
+	void cleanUp(SDL_Window* theSDLWND);
+	void render(SDL_Window* theSDLWND, SDL_Renderer* theRenderer);
+	void render(SDL_Window* theSDLWND, SDL_Renderer* theRenderer, double rotAngle, SDL_Point* spriteCentre);
+	void update();
+	void update(double deltaTime);
+	bool getInput(bool theLoop);
+	double getElapsedSeconds();
+
+	static cGame* getInstance();
+
+private:
+
+	static cGame* pInstance;
+	// for framerates
+	time_point< high_resolution_clock > m_lastTime;
+	time_point< high_resolution_clock > m_CurrentTime;
+	duration< double > deltaTime;
+
+	// game related variables
+	// Sprites for displaying background and rocket textures
+	cSprite spriteBkgd;
+	cRocket rocketSprite;
+	// Game objects
+	
+
+};
+
+#endif
